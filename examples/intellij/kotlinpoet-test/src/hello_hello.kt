bbbacbd3e57cf5693a26b8bbc2aedbfd9a54d5e4
Chris Koeberle <politedog@gmail.com>
2019-03-20 00:35:46 -0500

    Add HelloWorld example from square/kotlinpoet README

---
+++ examples/intellij/kotlinpoet-test/src/hello_hello.kt
@@ -0,0 +1,27 @@
+import com.squareup.kotlinpoet.*
+
+val greeterClass = ClassName("", "Greeter")
+val file = FileSpec.builder("", "HelloWorld")
+    .addType(
+        TypeSpec.classBuilder("Greeter")
+        .primaryConstructor(
+            FunSpec.constructorBuilder()
+            .addParameter("name", String::class)
+            .build())
+        .addProperty(
+            PropertySpec.builder("name", String::class)
+            .initializer("name")
+            .build())
+        .addFunction(FunSpec.builder("greet")
+            .addStatement("println(%P)", "Hello, \$name")
+            .build())
+        .build())
+    .addFunction(FunSpec.builder("main")
+        .addParameter("args", String::class, KModifier.VARARG)
+        .addStatement("%T(args[0]).greet()", greeterClass)
+        .build())
+    .build()
+
+fun main(vararg args: String) {
+    file.writeTo(System.out)
+}
