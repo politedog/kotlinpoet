9d9ae5ab36ad214870b0c3bde70652f015eebbd3
Chris Koeberle <politedog@gmail.com>
2019-03-20 10:38:13 -0500

    Add examples, work on presentation

--- examples/intellij/kotlinpoet-test/src/hello_hello.kt
+++ examples/intellij/kotlinpoet-test/src/hello_hello.kt
@@ -1,8 +1,28 @@
 import com.squareup.kotlinpoet.*
+import kotlin.reflect.KClass
+import kotlin.reflect.full.memberProperties
 
-val greeterClass = ClassName("", "Greeter")
-val file = FileSpec.builder("", "HelloWorld")
-    .addType(
+
+class Dependency() {
+    var name: String = ""
+    var color: String = ""
+    var shape: String = ""
+}
+
+
+fun writeClassInterface(kClass: KClass<out Any>) : String {
+    val name = kClass.simpleName?:"anon"
+    val dependencyClass = ClassName("", name)
+    val classTypeBuilder = TypeSpec.classBuilder(dependencyClass)
+    for(prop in kClass.memberProperties) {
+        classTypeBuilder.addProperty(prop.name, prop.returnType.asTypeName().copy(true))
+    }
+    val file = FileSpec.builder("", name)
+        .addType(classTypeBuilder.build())
+        .build()
+    return file.toString()
+}
+/*    .addType(
         TypeSpec.classBuilder("Greeter")
         .primaryConstructor(
             FunSpec.constructorBuilder()
@@ -20,8 +40,8 @@ val file = FileSpec.builder("", "HelloWorld")
         .addParameter("args", String::class, KModifier.VARARG)
         .addStatement("%T(args[0]).greet()", greeterClass)
         .build())
-    .build()
+    .build()*/
 
 fun main(vararg args: String) {
-    file.writeTo(System.out)
+    System.out.println(writeClassInterface(Dependency::class))
 }
---
+++ src-kotlinpoet.md
@@ -0,0 +1,48 @@
+# Kotlin code generation with kotlinpoet
+
+---
+
+# Chris Koeberle
+# Bottle Rocket 
+### @kodi
+
+---
+
+# What is kotlinpoet?
+
+A library from Square for writing Kotlin programs that have Kotlin source as output.
+
+* Takes care of things you'd rather not think about, like import statements
+* Requires you to learn a new syntax for writing about Kotlin
+
+---
+
+# Why kotlinpoet?
+
+* IDE Plugins
+* Create boilerplate based on data files
+* Create dependent code automatically
+* Skynet?
+
+---
+
+# Where kotlinpoet?
+
+<https://github.com/square/kotlinpoet>
+
+`compile 'com.squareup:kotlinpoet:1.1.0'`
+
+Easy places to get started:
+
+* [IntelliJ IDEA](https://www.jetbrains.com/idea/download/)
+* [kscript](https://github.com/holgerbrandl/kscript)
+
+---
+
+
+
+---
+
+# Sources
+
+<https://bit.ly/cdk-kotlinpoet>
